d2eae7447d897a14ff8f4d13ecbaf45962a1a61e
zilola-11 <asadbekpodsho@gmail.com>
2025-04-28 16:51:56 +0500

    homework01

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,8 @@
 #Create a variable 'x' and assign it the value of 10.
-
+x = 10
 #Create a variable 'y' and assign it the value of 5.
-
+y = 5
 #Create a variable 'result' and assign it the value of x + y.
-
+result = x + y
 #Print the value of the 'result'.
+print(result)
